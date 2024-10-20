70eb6a0f066e196a3b43b31bb16c1774bca79d0b
Staninna <26458805+Staninna@users.noreply.github.com>
2024-10-20 18:26:37 +0200

    configurable templateDir

--- src/Frame/Controller/BaseController.php
+++ src/Frame/Controller/BaseController.php
@@ -8,9 +8,10 @@ abstract class BaseController
     {
     }
 
-    public static function view(string $view, array $data = []): void
+    // TODO: Make config.php or smth to store the template directory among other things
+    public static function view(string $view, array $data = [], string $templateDir = 'views'): void
     {
         extract($data);
-        include __DIR__ . "/../../views/{$view}.php";
+        include __DIR__ . "/../../{$templateDir}/{$view}.php";
     }
 }
