e6ce9b0ca183f6d4cf58ca652aeef8cd84749052
Staninna <26458805+Staninna@users.noreply.github.com>
2024-10-20 23:04:24 +0200

    Able toi so db data trough models controllers and views

--- src/views/home.index.php
+++ src/views/home.index.php
@@ -5,8 +5,17 @@
     <title>Home</title>
 </head>
 <body>
-<h1>Home</h1>
+<h1>Tasks</h1>
 
-<p>Home page</p>
+<p>Tasks</p>
+
+<?php foreach ($tasks as $task): ?>
+    <h2><?= $task->title ?></h2>
+    <p><?= $task->beschrijving ?></p>
+    <p>Status: <?= $task->status ?></p>
+    <p>Verval datum: <?= $task->verval_datum ?></p>
+    <p>Prioriteit: <?= $task->prioriteit ?></p>
+<?php endforeach; ?>
+
 </body>
 </html>
