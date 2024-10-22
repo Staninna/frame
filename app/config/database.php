6960a92fc6a15ff85e2da78b8c31314d559ef64a
Staninna <26458805+Staninna@users.noreply.github.com>
2024-10-22 22:30:17 +0200

    Forgot i had commented something out

--- app/config/database.php
+++ app/config/database.php
@@ -6,7 +6,7 @@ return [
         'mysql' => [
             'driver' => 'mysql',
             'host' => getenv('DB_HOST') ?: 'localhost',
-//            'port' => getenv('DB_PORT') ?: 3306,
+            'port' => getenv('DB_PORT') ?: 3306,
             'database' => getenv('DB_NAME') ?: 'test',
             'username' => getenv('DB_USER') ?: 'root',
             'password' => getenv('DB_PASSWORD') ?: 'root',
