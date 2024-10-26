ccf5c8beb91aa2c77388ebe601b4ee52584ae3d6
Staninna <26458805+Staninna@users.noreply.github.com>
2024-10-26 14:43:26 +0200

    Removed todo

--- public/index.php
+++ public/index.php
@@ -9,9 +9,6 @@ use Frame\Boot\Bootstrap;
 
 $app = new Bootstrap();
 
-// TODO: Make seeder command/interface
-if (false) { require_once BASE_PATH . '/app/seed.php'; }
-
 $app->run();
 
 // TODO: Make functions.php in the framework for common functions to get shit out of the config and other common shit
