4fd2ffe538eeff3b9e26dda94e390505123adb9e
Staninna <26458805+Staninna@users.noreply.github.com>
2024-10-26 12:22:33 +0200

    Added new route

--- app/routes.php
+++ app/routes.php
@@ -3,6 +3,8 @@
 /* @var Router $router */
 
 use controllers\HomeController;
+use controllers\UsersController;
 use Frame\Router\Router;
 
 $router->get('/home', [HomeController::class, 'index'], 'home.index');
+$router->get('/test', [UsersController::class, 'index'], 'users.index');
---
+++ app/views/users.index.php
@@ -0,0 +1,19 @@
+<?php
+/** @var string $name */
+/** @var string $email */
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Users</title>
+</head>
+<body>
+<h1>Users</h1>
+
+<p>Name: <?= $name ?></p>
+<p>Email: <?= $email ?></p>
+</body>
+</html>
